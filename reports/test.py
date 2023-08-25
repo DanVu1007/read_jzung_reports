19646c1b0377bd3a37f174721f37b2299490190c
Danva <danva@arrowhitech.co>
2023-08-25 13:58:45 +0700

    add gitignore

---
+++ reports/test.py
@@ -0,0 +1,33 @@
+Đơn giá SL T.tiền
+
+1. Bánh trung thu nướng - 75g - Nhân
+thập cẩm
+
+30.000 x11 30.000
+
+2. Bánh trung thu nướng - 75g - Nhân
+dưa lưới
+
+33.000 x42 33.000
+
+Tạm tính 206.000 đ
+Chiết khấu -30.900 đ
+Tổng cộng 175.100đ
+Chưa thanh toán 175.100đ
+
+[
+    {
+        'name': 'Bánh trung thu nướng',
+        'weight': '75g',
+        'variant': 'Nhân thập cẩm',
+        'price': '30.000'
+        'qty': 11
+    }
+    {
+        'name': 'Bánh trung thu nướng',
+        'weight': '75g',
+        'variant': 'Nhân dưa lưới',
+        'price': '33.000'
+        'qty': 42
+    }
+]
